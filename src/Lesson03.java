aa95f8c11df02c83f01f322461f1a0d1d120b1ee
tomokoba <tomokoba64@gmail.com>
2021-05-07 23:09:23 +0900

    [Add]lessons

---
+++ src/Lesson03.java
@@ -0,0 +1,14 @@
+
+public class Lesson03 {
+	public static void main(String[] args) {
+		final int lucky_number = 7;
+		String str;
+		str = "tomokoba";
+		System.out.println(str);
+		
+		str = "Javaの勉強をはじめました";
+		System.out.println(str);
+		System.out.println(lucky_number);
+	}
+
+}
---
+++ src/Lesson04.java
@@ -0,0 +1,22 @@
+
+public class Lesson04 {
+	public static void main(String[] args) {
+		long l = 777777777777777L;
+		System.out.println(l);
+		double d = 3.14;
+		System.out.println(d);
+		boolean b = true;
+		System.out.println(b);
+		
+		Float f = 3.14141414f;
+		System.out.println(f);
+		Integer i = 777;
+		System.out.println(i);
+		Character c = 'あ';
+		System.out.println(c);
+		
+		int[] array = { 1, 2, 3, 4, 5 };
+		System.out.println(array[3]);
+	}
+
+}
