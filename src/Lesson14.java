f9f99d0fcc13ae387c8e3302861c9c5095ba2d11
tomokoba <tomokoba64@gmail.com>
2021-05-08 23:06:09 +0900

    [Add]lesson

---
+++ src/Lesson14.java
@@ -0,0 +1,8 @@
+
+public class Lesson14 {
+	public static void main(String[] args) {
+		String ages = "28";
+		int n = Integer.parseInt(ages);
+		System.out.println("あなたは来年、" + (n + 1) + "歳になりますね。");
+	}
+}
