532c6d65f4553b50abce055a8fd90fb4b3f39c92
tomokoba <tomokoba64@gmail.com>
2021-05-12 18:04:27 +0900

    [Add]Lesson

---
+++ src/Lesson0304.java
@@ -0,0 +1,13 @@
+
+public class Lesson0304 {
+	public static void main(String[] args) {
+		boolean tenki = false;
+		if(tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩に行きます");
+		} else {
+			System.out.println("DVDを見ます");
+			System.out.println("寝ます");
+		}
+	}
+}
