6f9df4ec98b3e5e7c2f37350185390b1047067b0
tomokoba <tomokoba64@gmail.com>
2021-05-13 22:43:04 +0900

    [Add]5章練習問題

--- src/Code0505.java
+++ src/Code0505.java
@@ -4,6 +4,7 @@ public class Code0505 {
 		add(100, 20);
 		add(200, 50);
 	}
+	// 複数の値を受け取るaddメソッド
 	public static void add(int x, int y) {
 		int ans = x + y;
 		System.out.println(x + "+" + y + "=" + ans);
