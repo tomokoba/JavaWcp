e2700fa7e54e514090857871d8d9d48b2fed3d3f
tomokoba <tomokoba64@gmail.com>
2021-05-13 21:46:45 +0900

    [Add]練習問題4.10

--- src/Code042.java
+++ src/Code042.java
@@ -2,11 +2,12 @@
 public class Code042 {
 	public static void main(String[] args) {
 		int[] moneyList = {121902, 8302, 55100};
-		
+
 		for(int i = 0; i < moneyList.length; i++) {
 			System.out.println(moneyList[i]);
 		}
-		
+
+		// 拡張for文
 		for(int value : moneyList) {
 			System.out.println(value);
 		}
