8ab18b814309221a05df32de4e25d15acb0d0c18
tomokoba <tomokoba64@gmail.com>
2021-05-16 01:49:32 +0900

    [Add]6章練習問題

---
+++ src/Lesson0601.java
@@ -0,0 +1,10 @@
+import comment.Zenhan;
+
+public class Lesson0601 {
+	public static void main(String[] args) throws Exception {
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		comment.Kouhan.callDeae();
+		comment.Kouhan.showMondokoro();
+	}
+}
